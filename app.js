94e50390a07b4b05a3ddd70297382fc3ee4b91e2
Harshal <ranjhaniharshal@gmail.com>
2022-04-06 12:32:33 +0530

    fix urls

--- app.js
+++ app.js
@@ -87,11 +87,11 @@ app.use(mongoSanitize());
 // app.use(helmet({ contentSecurityPolicy: false }));
 
 const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com",
-    "https://api.tiles.mapbox.com",
-    "https://api.mapbox.com",
-    "https://kit.fontawesome.com",
-    "https://cdnjs.cloudflare.com",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://fontawesome.com",
     "https://cdn.jsdelivr.net",
     "https://kit.fontawesome.com/",
     "https://ka-f.fontawesome.com/",
@@ -99,12 +99,11 @@ const scriptSrcUrls = [
 ];
 //This is the array that needs added to
 const styleSrcUrls = [
-    "https://kit-free.fontawesome.com",
-    "https://stackpath.bootstrapcdn.com",
-    "https://api.mapbox.com",
-    "https://api.tiles.mapbox.com",
-    "https://fonts.googleapis.com",
-    "https://use.fontawesome.com",
+    "https://kit-free.fontawesome.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
     "https://cdn.jsdelivr.net",
     "https://kit.fontawesome.com/",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
@@ -115,8 +114,10 @@ const styleSrcUrls = [
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
-    "https://*.tiles.mapbox.com",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
     "https://events.mapbox.com/",
+    "https://*.tiles.mapbox.com",
 ];
 const fontSrcUrls = [];
 app.use(
@@ -127,14 +128,13 @@ app.use(
             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
             workerSrc: ["'self'", "blob:"],
-            childSrc: ["blob:"],
             objectSrc: [],
             imgSrc: [
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-                "https://images.unsplash.com",
+                "https://res.cloudinary.com/ddeu2euos/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
         },
