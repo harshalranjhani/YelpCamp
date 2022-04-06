bef019780ab0a576a47523bda97c86b4e1476957
Harshal <ranjhaniharshal@gmail.com>
2022-04-06 11:18:55 +0530

    add start script

--- app.js
+++ app.js
@@ -159,7 +159,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err,statusCode });
 })
 
+const port = process.env.PORT || 3000
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log(`Listening on port ${port}`);
 })
 
