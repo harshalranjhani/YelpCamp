51378e80f218e0e2db8bf31a3a53bc17ab0ef505
Harshal Ranjhani <ranjhani@pop-os.localdomain>
2023-07-17 00:42:05 +0530

    fix: first reviewers

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,24 +2,32 @@ const Review = require("../models/review");
 const Campground = require("../models/campground");
 const User = require("../models/user");
 const sendMail = require("../utils/sendHTMLMail");
+const axios = require("axios");
 
 module.exports.createReview = async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   console.log(campground.reviews.length);
   const review = new Review(req.body.review);
   console.log(req.user);
-  if (campground.reviews.length === 1) {
+  if (campground.reviews.length === 0) {
     campground.firstReviewer = req.user._id;
     req.user.firstReviewCount = req.user.firstReviewCount + 1;
     await User.updateOne(
       { _id: req.user._id },
       { $set: { firstReviewCount: req.user.firstReviewCount } }
     );
-    await sendMail(
-      req.user.email,
-      "Congratulations! You're the first reviewer!",
-      `<h1>Hey ${req.user.username}!</h1><br/><p>We're so excited! You're the first reviewer for ${campground.title}!</p><br/><p>Thanks for using Yelpcamp, we hope you enjoy our services!</p>`
+    mailObj = {
+      email: req.user.email,
+      name: req.user.username,
+      campgroundName: campground.title,
+    };
+    const response = await axios.post(
+      "https://harshalranjhaniapi.vercel.app/mail/firstReview",
+      {
+        mailObj,
+      }
     );
+    console.log(response);
   }
   review.author = req.user._id;
   campground.reviews.push(review);
